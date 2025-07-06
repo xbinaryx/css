92323b73dd04e9f398296482f2e801687681858b
xbinaryx <xbinaryx68@gmail.com>
2025-07-06 19:19:56 +0300

    chore: automate generation of recommended rules config

--- src/index.js
+++ src/index.js
@@ -9,16 +9,8 @@
 
 import { CSSLanguage } from "./languages/css-language.js";
 import { CSSSourceCode } from "./languages/css-source-code.js";
-import noEmptyBlocks from "./rules/no-empty-blocks.js";
-import noDuplicateImports from "./rules/no-duplicate-imports.js";
-import noImportant from "./rules/no-important.js";
-import noInvalidProperties from "./rules/no-invalid-properties.js";
-import noInvalidAtRules from "./rules/no-invalid-at-rules.js";
-import noInvalidNamedGridAreas from "./rules/no-invalid-named-grid-areas.js";
-import preferLogicalProperties from "./rules/prefer-logical-properties.js";
-import relativeFontUnits from "./rules/relative-font-units.js";
-import useLayers from "./rules/use-layers.js";
-import useBaseline from "./rules/use-baseline.js";
+import recommendedRules from "./build/recommended-config.js";
+import rules from "./build/rules.js";
 
 //-----------------------------------------------------------------------------
 // Plugin
@@ -32,30 +24,11 @@ const plugin = {
 	languages: {
 		css: new CSSLanguage(),
 	},
-	rules: {
-		"no-empty-blocks": noEmptyBlocks,
-		"no-duplicate-imports": noDuplicateImports,
-		"no-important": noImportant,
-		"no-invalid-at-rules": noInvalidAtRules,
-		"no-invalid-named-grid-areas": noInvalidNamedGridAreas,
-		"no-invalid-properties": noInvalidProperties,
-		"prefer-logical-properties": preferLogicalProperties,
-		"relative-font-units": relativeFontUnits,
-		"use-layers": useLayers,
-		"use-baseline": useBaseline,
-	},
+	rules,
 	configs: {
 		recommended: {
 			plugins: {},
-			rules: /** @type {const} */ ({
-				"css/no-empty-blocks": "error",
-				"css/no-duplicate-imports": "error",
-				"css/no-important": "error",
-				"css/no-invalid-at-rules": "error",
-				"css/no-invalid-named-grid-areas": "error",
-				"css/no-invalid-properties": "error",
-				"css/use-baseline": "warn",
-			}),
+			rules: recommendedRules,
 		},
 	},
 };
